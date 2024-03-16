ae0ca45a41ccd6d9b7072b6938a51bccafb705a9
zimo <2558023586@qq.com>
2024-03-16 23:21:46 +0800

    项目基础搭建-样式重置

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+// 全局样式
+import "normalize.css";
+import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
